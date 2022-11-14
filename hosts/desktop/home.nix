07051d5678d2c0c20a258407f80163e2ef45e9f0
arphe42 <arphe42@gmail.com>
2022-11-14 04:20:52 -0500

    heaset buttons

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -24,4 +24,12 @@
   services = {
     blueman-applet.enable = true;
   };
+
+  # Bluetooth headset buttons
+  systemd.user.services.mpris-proxy = {
+    Unit.Description = "Mpris proxy";
+    Unit.After = [ "network.target" "sound.target" ];
+    Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+    Install.WantedBy = [ "default.target" ];
+  };
 }
